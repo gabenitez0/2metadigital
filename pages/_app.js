06a8f4b4eb11ea0c5db64d909d5ab5d10c91c25a
Gabriel <benitez.gabriel@hotmail.com.ar>
2022-03-11 14:03:57 -0300

    pixel

--- pages/_app.js
+++ pages/_app.js
@@ -62,21 +62,18 @@ function MyFunctionComponent({ children }) {
     });
   }
 
-  const router = useRouter()
-
-
   const advancedMatching = { em: 'some@email.com' }; // optional, more info: https://developers.facebook.com/docs/facebook-pixel/advanced/advanced-matching
   const options = {
     autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
     debug: false, // enable logs
   };
-  ReactPixel.init('yourPixelIdGoesHere', advancedMatching, options);
+  ReactPixel.init('1038198026550249', advancedMatching, options);
 
   ReactPixel.pageView(); // For tracking page view
   ReactPixel.track(event, data); // For tracking default events. More info about standard events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#standard-events
-  ReactPixel.trackSingle('PixelId', event, data); // For tracking default events.
+  ReactPixel.trackSingle('1038198026550249', event, data); // For tracking default events.
   ReactPixel.trackCustom(event, data); // For tracking custom events. More info about custom events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#custom-events
-  ReactPixel.trackSingleCustom('PixelId', event, data); // For tracking custom events.
+  ReactPixel.trackSingleCustom('1038198026550249', event, data); // For tracking custom events.
   
 
   return (
