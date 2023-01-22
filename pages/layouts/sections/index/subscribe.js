97ae8b4ad56e188b860faeb935f2c752e8cd4afc
Gabriel Benitez <benitez.gabriel@hotmail.com.ar>
2023-01-22 18:18:57 -0300

    Update subscribe.js

--- pages/layouts/sections/index/subscribe.js
+++ pages/layouts/sections/index/subscribe.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import {Container,Row,Col} from 'reactstrap'
+const link = {
+    color: 'var(--primary)',
+    display: 'block',
+    marginTop: 20,
+}
 const Subscribs = () => (
     <footer className="app1 subscribe bg">
         <Container>
@@ -7,10 +12,12 @@ const Subscribs = () => (
                 <Col lg="6" className="offset-lg-3">
                     <div className="title title1">
                         <div className="main-title">
-                            <h2 style={{lineHeight: 1.2}}>Queremos trabajar con vos y potenciar nuestros resultados juntos</h2>
+                            <h2 className="mb-4" style={{lineHeight: 1.2}}>Sobre nosotros</h2>
                         </div>
-                        <div className="sub-title">
-                            <p className="para">Somos Emprendedores Que Se Potencian Junto A Otros Emprendedores Para Alcanzar Todas Las Metas Trabajando En Equipo.</p>
+                        <div className="sub-title mb-5">
+                            <p className="para pb-3">Somos una agencia de marketing digital que brinda asesoramiento y soluciones tecnológicas a negocios en crecimiento que buscan tener presencia y destacar en internet, redes sociales y motores de búsqueda.</p>
+                            <p className="para"><b>Más de 5 años en el mercado y clientes satisfechos.</b></p>
+                            <a href="https://metadigital.com.ar" target="_blank" style={link}>Ir al sitio web principal →</a>
                         </div>
                     </div>
                 </Col>
@@ -33,25 +40,25 @@ const Subscribs = () => (
                         </div>
                     </div>
                 </Col>
-            </Row>
+            </Row>*/}
             <Row>
                 <Col md="12">
-                    <div className="socials-lists m-t-20">
+                    <div className="socials-lists">
                         <ul className="socials-horizontal justify-content-center">
+                            <li>
+                                <a href="https://www.instagram.com/paginasweb_arg/" target="_blank">
+                                    <i aria-hidden="true" className="fa fa-instagram center-content"></i>
+                                </a>
+                            </li>
                             <li>
                                 <a href="https://www.instagram.com/2metadigital/" target="_blank">
                                     <i aria-hidden="true" className="fa fa-instagram center-content"></i>
                                 </a>
                             </li>
-                            <li>
-                                <a  href="https://www.facebook.com/2metadigital/" target="_blank">
-                                    <i aria-hidden="true" className="fa fa-facebook center-content"></i>
-                                </a>
-                            </li>
                         </ul>
                     </div>
                 </Col>
-            </Row>*/}
+            </Row>
         </Container>
     </footer>
 )
