62a79c13d135fd8e898c3e77f9b8cd08baf0b09b
Gabriel Benitez <benitez.gabriel@hotmail.com.ar>
2023-01-22 18:18:41 -0300

    Update footer.js

--- pages/layouts/sections/index/footer.js
+++ pages/layouts/sections/index/footer.js
@@ -10,7 +10,7 @@ const Footer = () => (
                     <Col xs="12">
                         <div className="center-content">
                             <div>
-                                <h6 className="copyright-text text-white">Copyright © 2022, Todos Los Derechos Reservados Por 2MetaDigital</h6>
+                                <h6 className="copyright-text text-white">Copyright © 2023. Todos los derechos reservados por <a className="text-white" href="https://metadigital.com.ar" target="_blank">MetaDigital</a> y <a className="text-white" href="https://www.instagram.com/paginasweb_arg/" target="_blank">@paginasweb_arg</a></h6>
                             </div>
                         </div>
                     </Col>
