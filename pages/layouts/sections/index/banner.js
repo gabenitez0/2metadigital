65f0c23793c3a5e1eef37213f775bcdf666adef8
Gabriel Benitez <benitez.gabriel@hotmail.com.ar>
2023-01-22 18:20:31 -0300

    Update banner.js

--- pages/layouts/sections/index/banner.js
+++ pages/layouts/sections/index/banner.js
@@ -13,18 +13,18 @@ const Banner = ({link}) => {
                         <div className="center-text" style={{marginTop: 50}}>
                             <div>
                                 <div className="header-text">
-                                    <h1><b>Vendé tus servicios en internet con una Web de Alto Impacto</b></h1>
+                                    <h1><b>Transformá tu negocio con una página web profesional y atractiva: atraé más clientes y aumentá tus ventas hoy!</b></h1>
                                 </div>
-                                <div className="header-sub-text">
-                                    <p className="text-white p-light">Permití a tus potenciales clientes conocer el VALOR de tu producto o servicio con una web de alta calidad y textos persuasivos para ayudarlos a tomar la decisión de compra e <b style={{fontWeight:600}}>incrementar tus ventas</b>.</p>
+                                <div className="mb-5">
+                                    <p className="text-white p-light">No pierdas más oportunidades de negocio: obtené una página web profesional y <b style={{fontWeight:600}}>destacá entre la competencia</b>.</p>
                                 </div>
-                                    <a className="btn btn-default btn-white btnBig" href={link} onClick={() => fbq('track', 'Lead')} target="_blank">Quiero contactarme <i aria-hidden="true" className="fa fa-whatsapp m-l-5"></i></a>
+                                    <a className="btn btn-default btn-white btnBig" href={link} onClick={() => fbq('track', 'Lead')} target="_blank">Me interesa <i aria-hidden="true" className="fa fa-whatsapp m-l-5"></i></a>
                             </div>
                         </div>
                     </Col>
                     <Col xl="5" lg="6" md="4">
                         <div className="img-mobile set-abs" style={{bottom: '0'}}>
-                            <Image alt="mockup header 2metadigital celular con web institucional"
+                            <Image alt="pagina web profesional metadigital"
                             data-aos="fade-left"
                             height={661}
                             width={450}
