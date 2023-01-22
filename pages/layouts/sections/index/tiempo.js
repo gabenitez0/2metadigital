05aeab6bcd0b35a73dadd01a863acb63273689ac
Gabriel Benitez <benitez.gabriel@hotmail.com.ar>
2023-01-22 18:19:29 -0300

    Update tiempo.js

--- pages/layouts/sections/index/tiempo.js
+++ pages/layouts/sections/index/tiempo.js
@@ -12,23 +12,24 @@ const Price = () => (
                 <Col md="10"  className="offset-md-1">
                     <div className="title" data-aos="fade-right" data-aos-delay="200">
                         <div className="main-title">
-                            <h2 style={{lineHeight:1.2}}><b>Imaginá los beneficios de este salto de calidad para tu negocio...</b></h2>
+                            <h3 className="mb-4" style={{fontWeight:800}}>Imaginá los beneficios para vos y tu negocio...</h3>
                         </div>
                         <div className="sub-title">
                             <ul className="sub-title-para">
-                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> <b>... tus potenciales clientes ahora te van a tomar en serio.</b></p></li>
-                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> <b>... podrás realmente transmitir el valor de tu producto o servicio.</b></p></li>
-                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> <b>... una nueva imagen profesional y de alto valor.</b></p></li>
-                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> ... la accesibilidad que les podrás brindar a tu tráfico.</p></li>
-                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> ... la cantidad de tiempo que vas a ahorrar de tareas repetitivas.</p></li>
-                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> ... cómo vas a poder mejorar tu producto y tu negocio con estadísticas.</p></li>
-                                <li><p className="sub-title-para"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> ... que ni siquiera te tengas que ocupar de la generación de contenido.</p></li>
+                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i><b> ... aparecerás en los principales motores de búsqueda como Google.</b></p></li>
+                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> <b>... podrás automatizar procesos de marketing y ventas.</b></p></li>
+                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> <b>... tus clientes podrán encontrarte y comprar en cualquier momento 24/7.</b></p></li>
+                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> <b>... tendrás una nueva imagen profesional y de alto valor.</b></p></li>
+                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> ... tendrás disponibilidad 24/7 para todos los dispositivos.</p></li>
+                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> ... ahorrarás hasta 24 horas semanales de tareas repetitivas.</p></li>
+                                <li><p className="sub-title-para pb-1"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> ... tendrás acceso a estadísticas y analíticas sobre tu público.</p></li>
+                                <li><p className="sub-title-para"><i aria-hidden="true" className="fa fa-check-circle mr-1" style={{color:'var(--primary)'}}></i> ... podrás transmitir el valor de tu producto o servicio.</p></li>
                             </ul>
                         </div>
                     </div>
                     <div className="title" data-aos="fade-left" data-aos-delay="200">
                         <div className="main-title">
-                            <h2><b>Tu proyecto nos importa:</b></h2>
+                            <h3 className="mb-4 mt-5" style={{fontWeight:800}}>Tu proyecto nos importa</h3>
                         </div>
                         <div className="sub-title">
                             <p className="sub-title-para"><b>Para conseguir los resultados que ofrecemos en esta página, hacemos una investigación de mercado de tu proyecto y análisis de tu producto o servicio, de esta forma nosotros podemos comprender el valor y el factor diferencial del mismo y presentarlo de una manera profesional y enfocada a resultados!</b></p>
@@ -36,7 +37,7 @@ const Price = () => (
                     </div>
                     <div className="title" data-aos="fade-right" data-aos-delay="200">
                         <div className="main-title">
-                            <h2><b>Vos nos importas:</b></h2>
+                        <h3 className="mb-4 mt-5" style={{fontWeight:800}}>Vos nos importas</h3>
                         </div>
                         <div className="sub-title">
                             <p className="sub-title-para"><b>Como emprendedores comprendemos perfectamente lo valioso de tu tiempo, es por eso que solo te vamos a sacar lo justo y necesario para hacer nuestro trabajo, de todo el resto nos encargamos nosotros!</b></p>
