a6cab19bc37244ba8a3013892d3adf669edc31bd
Gabriel Benitez <benitez.gabriel@hotmail.com.ar>
2023-01-23 23:36:34 -0300

    Update menu.js

--- constant/menu.js
+++ constant/menu.js
@@ -1,36 +1,28 @@
 export const MENUITEMS = [
    {
-      title: 'Inicio', type: 'sub', link: '#', children: [
-         { path: '/', title: 'Inicio', type: 'link' },
-         { path: '/#Servicios', title: 'Qué hacemos', type: 'link' },
-         { path: '/#Proyectos', title: 'Proyectos', type: 'link' },
-         { path: '/#Calificaciones', title: 'Calificaciones', type: 'link' },
+      title: 'Nosotros', type: 'sub', link: '#', children: [
+         { path: '/#paginasweb', title: 'Cómo trabajamos', type: 'link' },
+         { path: '/#casosexito', title: 'Casos de éxito', type: 'link' },
+         { path: '/#contacto', title: 'Lista de precios', type: 'link' },
+         { path: '/#contacto', title: 'Contacto', type: 'link' },
+         { path: '/#nosotros', title: 'Sobre nosotros', type: 'link' },
       ],
    },
    {
       title: 'Servicios', type: 'sub', link: '#', children: [
-         { path: '/ecommerce', title: 'E-commerces', type: 'link' },
-         { path: '#', title: 'Landing Pages', type: 'link' },
-         { path: '#', title: 'Institucionales', type: 'link' },
-         { path: '#', title: 'Funnels de Venta', type: 'link' },
-         { path: '#', title: 'Diseño Web', type: 'link' },
-         { path: '#', title: 'Diseño Gráfico', type: 'link' },
-         { path: '#', title: 'Copywriting', type: 'link' },
-         { path: '#', title: 'Mantenimiento', type: 'link' },
-         { path: '#', title: 'Dominios y SSL', type: 'link' },
-         { path: '#', title: 'Hosting y DNS', type: 'link' },
+         { path: '#', title: 'Web E-commerce', type: 'link' },
+         { path: '#', title: 'Web Landing Page', type: 'link' },
+         { path: '#', title: 'Web Institucional', type: 'link' },
+         { path: '#', title: 'Generación de Contenido Web', type: 'link' },
+         { path: '#', title: 'Posicionamiento en Google', type: 'link' },
+         { path: '#', title: 'Publicidad en Google', type: 'link' },
+         { path: '#', title: 'Hosting y Mantenimiento', type: 'link' },
       ],
    },
    {
       title: 'Contacto', type: 'sub', link: '#', children: [
-         { path: '#contacto', title: 'Iniciar Chat', type: 'link' },
-         { path: '#contacto', title: 'Contactar', type: 'link' }
+         { path: '/#contacto', title: 'Iniciar Chat', type: 'link' },
+         { path: '/#contacto', title: 'Contactar', type: 'link' }
       ],
-   },
-   {
-      title: 'Afiliados', type: 'sub', link: '#', children: [
-         { path: '/afiliados', title: 'Programa de Afiliados', type: 'link' },
-         { path: '/afiliados', title: 'Trabaja con nosotros', type: 'link' }
-      ],
-   },
+   }
 ]
